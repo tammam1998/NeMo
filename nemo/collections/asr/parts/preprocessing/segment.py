02c1ee8f313ff0e139a52be1bb45236ae6b1421a
Tammam Mustafa <tammammusatafa@gmail.com>
2025-02-18 04:37:49 +0000

    Fix compatibility issue with numpy 2.0

--- nemo/collections/asr/parts/preprocessing/segment.py
+++ nemo/collections/asr/parts/preprocessing/segment.py
@@ -257,13 +257,13 @@ class AudioSegment(object):
     def _convert_samples_to_float32(samples):
         """Convert sample type to float32.
         Audio sample type is usually integer or float-point.
-        Integers will be scaled to [-1, 1] in float32.
+        Integers will be scaled tos [-1, 1] in float32.
         """
         float32_samples = samples.astype('float32')
-        if samples.dtype in np.sctypes['int']:
+        if samples.dtype in [np.int8, np.int16, np.int32, np.int64]:
             bits = np.iinfo(samples.dtype).bits
             float32_samples *= 1.0 / 2 ** (bits - 1)
-        elif samples.dtype in np.sctypes['float']:
+        elif samples.dtype in [np.float16, np.float32, np.float64]:
             pass
         else:
             raise TypeError("Unsupported sample type: %s." % samples.dtype)
